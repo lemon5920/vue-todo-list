0815dbdf4f6c94a18d466edb70fdbc3bfb785e66
BlueT - Matthew Lien - 練喆明 <BlueT@BlueT.org>
2017-05-04 00:43:03 +0800

    clean, performance, API improvement improve: performance changed: in-memory structure fixed: RESTful API

--- backend/express/routes/todos.js
+++ backend/express/routes/todos.js
@@ -1,45 +1,46 @@
 var express = require('express');
+var filter = require('obj-filter');
 var router = express.Router();
+
 var todoIndex = 0;
-var todos = [];
+var todos = {};
+var tpl = {
+  id: "todo id",
+  content: "todo content",
+  status: false
+};
 
 router.get('/', function(req, res, next) {
   res.json(todos);
 });
 
 router.post('/', function(req, res, next) {
-  let addTodo = {
-    "id": todoIndex++,
-    "content": req.body.content,
-    "done": false
-  }
-
-  if(todos.push(addTodo)) {
+  let newTodo = filter.merge( tpl, {
+    "id": ++todoIndex,
+    "content": req.body.content
+  });
+  
+  if (typeof todos[newTodo.id] === 'undefined') {
+    todos[newTodo.id] = newTodo;
     return res.json(todos);
+  } else {
+    return res.json({err: 'id conflict'});
+    throw new Error('id conflict');
   }
-  return "error";
 });
 
 router.delete('/:id', function(req, res, next) {
   var id = req.params.id;
-  var index = todos.findIndex(function(el) {
-    return el.id == id;
-  })
-  if(todos.splice(index, 1)) {
-    return res.json(todos);
-  }
-  return "error";
+  
+  delete todos[id];
+  return res.json(todos);
 });
 
 router.patch('/:id', function(req, res, next) {
   var id = req.params.id;
-  var index = todos.findIndex(function(el) {
-    return el.id == id;
-  })
-  if(todos[index].done = !todos[index].done) {
-    return res.json(todos);
-  }
-  return "error";
+  
+  todos[id] = filter.merge(todos[id], req.body);
+  return res.json(todos);
 });
 
 module.exports = router;
